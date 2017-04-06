7e31e5ddc20ed2819f65c3089d36b1d44d826668
Pascal Larive <pascallarive@aol.com>
2017-04-06 13:58:11 +0000

    Add Projet MongoDB Model

---
+++ models/Projet.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var ProjetSchema = new Schema({
+    chatID : Number,
+    participants : Array(100),
+    ressources : Array(100),
+    description : String,
+    gantt : Number
+});
+
+ProjetSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('Projet', ProjetSchema);
