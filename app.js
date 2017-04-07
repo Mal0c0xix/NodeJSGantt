1deb0e5a5eeae3798bbbe0cd42aab2c5a9272782
Maxime R <maxime.rome@ynov.com>
2017-04-07 20:52:08 +0000

    Mise à jour de la disposition du contenu du serveur (plus clair et commenté), ajout de socket.io et début d'implémentation

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var app = express();
-var http = require('http').Server(app);
-var LocalStrategy = require('passport-local').Strategy;
+'use strict'
+// Gestion des middlewear
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
 
-var port = process.env.PORT || '3000';
+// Base de données et connexion
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const logger = require('morgan');
 
+// Temps réel sur le gantt
+const async = require('async');
+const socketio = require('socket.io');
+
+// Affichage
+const favicon = require('serve-favicon/');
+
+// Définition du port
+const port = process.env.PORT || '3000';
+
+// Gestion de la connexion BD
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/node-auth')
   .then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Import de la gestion des routes
+const index = require('./routes/index');
+const users = require('./routes/users');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
+// A décommenter quand le favicon sera dans /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Configuration de express
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,33 +54,60 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Définitions des routes express
 app.use('/', index);
 app.use('/users', users);
 
-// passport configuration
-var User = require('./models/User');
+// Passport configuration
+const User = require('./models/User');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// catch 404 and forward to error handler
+// Gestion io et temps réel avec les gantts
+var server = require('http').createServer(app);
+var io = socketio.listen(server);
+var sockets = [];
+// Quand il y a une connection à un gantt
+io.on('connection', function (socket) {
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+    });
+    
+    // Mise à jour du gantt -> broadcast
+    socket.on('ganttUpdate', function (msg) {
+        broadcast('ganttUpdate', msg);
+      });
+});
+
+// Fonction pour envoyer la mise à jour à tous les utilisateurs
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    socket.emit(event, data);
+  });
+}
+
+// Gère tous les cas de page non connues
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Permet d'obtenir les érreurs et de débugger
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Permet d'envoyer des logs en local afin de débugger
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Envoi de la page d'érreur
   res.status(err.status || 500);
   res.render('error');
 });
 
+// Le serveur écoute sur le port 3000
 http.listen(port, () => {
   console.log("Serveur en écoute sur 127.0.0.1:", port);
 })
